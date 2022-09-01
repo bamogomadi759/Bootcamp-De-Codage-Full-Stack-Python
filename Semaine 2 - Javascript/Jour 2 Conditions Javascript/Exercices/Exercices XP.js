688a49c12941e6c1628e689b5f704cceb87fbc2b
bamogomadi759 <bamogomadi759@gmail.com>
2022-09-01 13:22:05 +0000

    Exercice XP

---
+++ Semaine 2 - Javascript/Jour 2 Conditions Javascript/Exercices/Exercices XP.js
@@ -0,0 +1,61 @@
+// 🌟 Exercice 1 : Instruction If/Else Simple
+
+let x = 5;
+let y = 2;
+if (x < y){
+    console.log("y est le plus grand nombre");
+}else  {
+    console.log("x est le plus grand nombre");
+}
+// x est le plus grand nombre
+
+
+// 🌟 Exercice 2 : Chihuahua
+
+// Créez une variable appelée newDogdont la valeur est "Chihuahua".
+newDog = "Chihuahua";
+'Chihuahua'
+
+// Vérifiez et affichez le nombre de lettres dans newDog.
+newDog.length
+// 9
+
+// Affichez la newDogvariable en majuscule 
+newDog.toUpperCase()
+'CHIHUAHUA'
+
+// Affichez la newDogvariable en minuscule
+
+newDog.toLowerCase()
+'chihuahua'
+
+// Vérifiez si la variable newDogest égale à "Chihuahua"
+
+if (newDog ===  "Chihuahua") {
+    console.log("J'adore les chihuahuas, c'est ma race de chien préférée");
+} else {
+    console.log ("Je m'en fous, je préfère les chats");
+}
+// J'adore les chihuahuas, c'est ma race de chien préférée
+
+
+
+// 🌟 Exercice 3 : Pair Ou Impair
+
+// Invitez l'utilisateur à entrer un nombre et enregistrez-le dans une variable.
+
+var num = Number(prompt("Entrer un nombre"));
+if (num % 2 == 0) {
+    console.log(num," est un nombre pair");
+} else {
+    console.log(num,"est un nombre impair");
+}
+// 25 'est un nombre impair'
+
+
+
+
+// 🌟 Exercice 4 : Discussion De Groupe
+
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
