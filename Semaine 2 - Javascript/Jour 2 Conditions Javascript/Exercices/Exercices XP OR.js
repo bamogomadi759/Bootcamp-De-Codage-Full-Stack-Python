b853d2b3337cd4ce628cefd070cbbe5c8e0bb392
bamogomadi759 <bamogomadi759@gmail.com>
2022-09-03 16:44:42 +0000

    Three langing pages

---
+++ Semaine 2 - Javascript/Jour 2 Conditions Javascript/Exercices/Exercices XP OR.js
@@ -0,0 +1,37 @@
+// Exercice 1 : Le Traducteur Du Monde
+
+// Demandez à l'utilisateur quelle langue il parle.
+
+let langue = prompt("Quelle langue parlez-vous ?")
+
+let langue = langue.toLowerCase()
+undefined
+if (langue==="français") {
+    console.log("Bonjour");
+} else if (langue==="anglais") {
+    console.log("Hello");
+} else if (langue==="hébreu") {
+    console("Shalom");
+}else {
+    console.log("01110011 01101111 01110010 01110010 01111001");
+}
+
+
+
+// Exercice 2 : L'assignateur De Notes
+
+var note = Number(prompt("Veuillez entrer une note"));
+if (note > 90) {
+  console.log("A");
+} else if (note > 80){
+  console.log("B");
+} else if (note > 70){
+  console.log("C");
+} else{
+  console.log("D");
+}
+
+
+
+// Exercice 3 : Verbe
+
